894245c2e20c4b13d0a53150c126bf8c76bf8657
Elaina-AFK <khglhk@yahoo.com>
2022-01-11 19:21:46 +0700

    lab15_2.cpp edited

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,41 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double a[][N]){
+    for(int i = 0; i < N; i++){
+       cout << "Row " << i+1 << ": ";
+       for(int l = 0; l < N; l++){
+        cin >> a[i][l]; 
+       }
+    }
+}
+
+void findLocalMax(const double b[][N], bool c[][N]){
+	for(int i = 1; i < N - 1; i++){
+		for(int j = 1; j < N - 1; j++){
+			if(b[i][j]>=b[i][j-1] && b[i][j] >= b[i-1][j] && b[i][j] >= b[i+1][j] && b[i][j] >= b[i][j+1]){
+				c[i][j] = 1;
+			}
+			else{
+				c[i][j] = 0;
+			}
+		}
+	}
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			if (i == 0 || j == 0 || i == N-1 || j == N-1){
+				c[i][j] = 0;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool d[][N]){
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < N; j++){
+			cout << d[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
